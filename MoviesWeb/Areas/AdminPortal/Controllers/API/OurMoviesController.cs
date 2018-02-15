6fb889e528aaadca551ea8104d69bffdc8f58ee5
taitMdaka <ttmdaka@gmail.com>
2018-02-15 09:17:35 +0200

    API Continued

---
+++ MoviesWeb/Areas/AdminPortal/Controllers/API/OurMoviesController.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using MediatR;
+using Movies.Backend.Core.Infrastructure;
+using MoviesWeb.Models;
+using OurMovies.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MoviesWeb.Areas.AdminPortal.Controllers.API
+{
+    public class OurMoviesController : ApiController 
+    {
+        private static IMediator _mediator;
+
+        public OurMoviesController()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(typeof(ListMoviesQuery).Assembly).AsImplementedInterfaces();
+            //builder.RegisterAssemblyTypes(typeof(UploadFileByByteCommand).Assembly).AsImplementedInterfaces();
+            _mediator = BootstrapMediatr.BuildMediator(builder);
+        }
+        [HttpGet]
+        public List<OurMovies.ViewModel.MoviesViewModel> ListMovies()
+        {
+            var listMoviesQuery = new ListMoviesQuery
+            {
+                //WithHiden = true
+
+
+            };
+            return _mediator.Send(listMoviesQuery).Result;
+        }
+    }
+}
